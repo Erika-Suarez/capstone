d55aa70d5595bde09ad67260e855162f8f9f0fb7
Erika Suarez <suarez.erika11@gmail.com>
2021-04-13 11:17:36 -0700

    set heroku deployment settings

--- app.js
+++ app.js
@@ -138,7 +138,8 @@ app.use("/", authRoutes)
         res.status(status).render("error", {err});
     });
     
-     
-app.listen(3000, () => {
-    console.log('listen on port 3000')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
